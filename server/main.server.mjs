51a4f25cc43963543edaf8508964d33c26ab6948
xuser <xuser@xsystem>
2024-04-02 17:10:08 +0100

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{b as a}from"./chunk-ZVIQNZJL.mjs";import"./chunk-BW2B2XIW.mjs";import"./chunk-RAROTAMY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
